8bb8e230d01de40bf3e59f6f728f2d9fb387dd40
chaturprani114 <pranav.chaturvedi@lumberfi.com>
2026-03-17 07:55:32 +0530

    Add fetch function to load articles from JSON

---
+++ script.js
@@ -0,0 +1,21 @@
+fetch("data/articles.json")
+.then(response => response.json())
+.then(data => {
+
+    const tableBody = document.querySelector("#articles-table tbody");
+
+    data.forEach(article => {
+
+        const row = document.createElement("tr");
+
+        row.innerHTML = `
+            <td>${article.date}</td>
+            <td>${article.title}</td>
+            <td>${article.author}</td>
+            <td><a href="${article.link}" target="_blank">Open</a></td>
+        `;
+
+        tableBody.appendChild(row);
+    });
+
+});
